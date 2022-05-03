0f3c547f8bb50733b409bb3e20f7523016f8d7b7
monir3430 <miamonir3430@gmail.com>
2022-05-03 22:33:51 +0600

    firebase auth

--- src/components/Authentication/Signin.js
+++ src/components/Authentication/Signin.js
@@ -1,10 +1,51 @@
 import React, { useState } from 'react';
+import {auth} from "../../firebase/firebase.init"
+import { toast, ToastContainer } from 'react-toastify';
 import "./Signin.css"
+import {useSignInWithEmailAndPassword} from "react-firebase-hooks/auth";
 
 const Signin = () => {
 
-    const [email, setEmail] = useState("")
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
 
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        hookError,
+      ] = useSignInWithEmailAndPassword(auth);
+
+    const handleEmailChange = (e)=>{
+        const emailRegularExpression = /\S+@\S+\.\S+/;
+        const validEmailTest= emailRegularExpression.test(e.target.value);
+        if(validEmailTest){
+            setEmail(e.target.value);
+            setError("");
+        }
+        else{
+            setError('Invalid Email');
+            setEmail("");
+        }
+    }
+
+    const handlePasswordChange = (e)=>{
+        const passwordRegularExpression = /.{6,}/;
+        const validPasswordTest = passwordRegularExpression.test(e.target.value);
+
+        if(validPasswordTest){
+            setPassword(e.target.value);
+            setError("");
+        }
+        else{
+            setError('At least 6 Characters');
+            setPassword("")
+        }
+
+
+    }
 
 
 
@@ -16,6 +57,10 @@ const Signin = () => {
 
     const handleSubmit = (e)=>{
         e.preventDefault();
+        console.log(email, password)
+        signInWithEmailAndPassword(email, password)
+
+        
     }
 
 
@@ -23,10 +68,15 @@ const Signin = () => {
         <div className="signin-area" onSubmit={handleSubmit}>
         <div className="signin">Please Sign in</div> <br />
         <form className="Signin-form" >
-            <input type="email" placeholder="Enter Email" />
-            <input type="password" placeholder=" Enter Password"  /> <br />
+            <input type="email" placeholder="Enter Email" onChange={handleEmailChange} />
             
-            <button>Signin</button>
+            <input type="password" placeholder=" Enter Password" onChange={handlePasswordChange} /> <br />
+            
+            <button>Signin</button> <br />
+            <small className='error'>{error}</small>
+            
+          
+            <ToastContainer />
            
 
             <p>No account?  <b>Sign-up</b> </p>
